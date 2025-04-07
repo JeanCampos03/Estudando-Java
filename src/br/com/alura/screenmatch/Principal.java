9b9fbbb4b9872df73041143378908e712849a6fb
Jean Campos <camposjean4@gmail.com>
2025-04-06 23:32:04 -0300

    consertando

--- src/br/com/alura/screenmatch/Principal.java
+++ src/br/com/alura/screenmatch/Principal.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
@@ -26,21 +28,31 @@ public class Principal {
         outro.exibeFichaTecnica();
 
 
-            Serie serie = new Serie();
-        serie.setNome("La Casa de Papel");
-        serie.setAnoDeLancamento(2017);
-        serie.setIncluidoNoPlano(true);
-        serie.setAtiva(true);
-        serie.setTemporadas(5);
-        serie.setEpisodiosPorTemporada(10);
-        serie.setMinutosPorEpisodio(45);
-        serie.exibeFichaTecnica();
+            Serie serieUm = new Serie();
+        serieUm.setNome("La Casa de Papel");
+        serieUm.setAnoDeLancamento(2017);
+        serieUm.setIncluidoNoPlano(true);
+        serieUm.setAtiva(true);
+        serieUm.setTemporadas(5);
+        serieUm.setEpisodiosPorTemporada(10);
+        serieUm.setMinutosPorEpisodio(45);
+        serieUm.exibeFichaTecnica();
 
         CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
         calculadora.inclui(favorito);
         calculadora.inclui(outro);
-        calculadora.inclui(serie);
+        calculadora.inclui(serieUm);
 
         System.out.println("Tempo total: " +calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtroUm = new FiltroRecomendacao();
+        filtroUm.filtra(favorito);
+
+        Episodio episodioUm = new Episodio();
+        episodioUm.setNumero(1);
+        episodioUm.setSerie(serieUm);
+        episodioUm.setTotalVisualizacoes(300);
+        filtroUm.filtra(episodioUm);
+
 }
 }
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme extends Titulo {
+import br.com.alura.screenmatch.calculos.Classificavel;
+
+public class Filme extends Titulo implements Classificavel {
 
     private String diretor;
 
@@ -18,4 +20,8 @@ public class Filme extends Titulo {
         System.out.println("----------------------------------------------");
     }
 
+    @Override
+    public int getClassificacao() {
+        return (int) pegaMedia() / 2;
+    }
 }
